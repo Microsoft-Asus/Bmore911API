52bca9f129be569fb2e1746d4708bf7a8112f5a2
Arnold Balliu <arnold.balliu@gmail.com>
2017-12-25 23:40:13 -0500

    completed v0.9.1 issues 1 through 4

---
+++ database/migrations/2017_12_26_040221_create_calls_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('time');
+            $table->unsignedTinyInteger('priority');
+            $table->string('district');
+            $table->string('description');
+            $table->string('bpd_call_id');
+            $table->string('address');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+}

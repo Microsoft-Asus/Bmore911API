b6656af371dc8148b6855daee1fe8a8b623f4190
Arnold Balliu <arnold.balliu@gmail.com>
2018-01-09 15:57:56 -0500

    testing deployment

--- app/AppStatics.php
+++ app/AppStatics.php
@@ -11,7 +11,6 @@ namespace App;
 
 class AppStatics
 {
-	
     public static $CALL_RECORDS_FILENAME = 'call_records.csv';
     public static $CALL_RECORDS_FILENAME_MINI = 'mini_call_records.csv';
     public static $UNKNOWN_STRING = 'UNKNOWN';
